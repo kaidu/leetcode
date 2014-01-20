3eca9aacef4cf005259ea58d0dc8c462343f0843
kaidurice <kaidusjtu@gmail.com>
2014-01-20 12:24:55 -0800

    Update simplifyPath.java

--- simplifyPath.java
+++ simplifyPath.java
@@ -1,60 +1,40 @@
 public class Solution {
-   		    public String simplifyPath(String path) {
-		        // IMPORTANT: Please reset any member data you declared, as
-		        // the same Solution instance will be reused for each test case.
-		        if (path.length() == 0) return "/";
-		        
-		        int n = path.length();
-		        Deque<String> dq = new ArrayDeque<String>();
-		        int i = 0;
-		        int start = 0;
-		        while (i < n && path.charAt(i) == '/') {
-		            i++;
-		            start++;
-		        }
-		        
-		        while (i < n) {
-		            while (i < n && path.charAt(i) != '/') i++;
-		            
-		            if (start < i) {
-		                String curr = path.substring(start, i);
-		                if (curr.equals("..")) {
-		                    if(!dq.isEmpty()) dq.removeLast();    
-		                }
-		                else if (!curr.equals(".")) {
-		                    dq.addLast(curr);
-		                }
-		                i++;
-		                start = i;
-		                
-		            }
-		            else {
-		                start++;
-		                i++;
-		            }
-		        }
-		        
-		        // print out the result.
-		        StringBuilder sb = new StringBuilder();
-		        while (!dq.isEmpty()) {
-		            sb.append('/');
-		            String curr = dq.getFirst();
-		            dq.removeFirst();
-		            sb.append(curr);
-		        }
-		        if (sb.length() == 0) {
-		            sb.append('/');
-		        }
-		        
-		        return sb.toString();
-		    }
-		    
-		    private boolean hasOnlyPoints(String str) {
-		    	if (str.length() == 0) return false;
-		    	
-		    	for (int i = 0; i < str.length(); i++) {
-		    		if (str.charAt(i) != '.') return false;
-		    	}
-		    	return true;
-		    }
+  public String simplifyPath(String path) {
+	        if (path == null || path.length() == 0) return "/";
+	        int n = path.length();
+	        
+	        Deque<String> dq = new ArrayDeque<String>();
+	        int start = 0;
+	        int end = 0;
+	        while (start+1 < n) {
+	            end = path.indexOf('/', start+1);
+	            if (end == -1) end = n;
+	            
+	            if (end == start+1) {
+	                start = end;
+	                continue;
+	            }
+	            
+	            String str = path.substring(start+1, end);
+	            if (str.equals("..")) {
+	                if (!dq.isEmpty()) {
+	                    dq.pollLast();
+	                }
+	            }
+	            else if (!str.equals(".")) {
+	                dq.offerLast(str);
+	            }
+	            
+	            start = end;
+	        }
+	        
+	        if (dq.isEmpty()) return "/";
+	        StringBuilder sb = new StringBuilder();
+	        while (!dq.isEmpty()) {
+	            sb.append('/');
+	            sb.append(dq.pollFirst());
+	        }
+	        
+	        return sb.toString();
+	    }
 }
